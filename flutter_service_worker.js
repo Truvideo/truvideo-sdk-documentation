d4214e0379273a68d5f768794b5b5a502b57bdb7
federico-amura-kenility <federico.amura@kenility.com>
2023-08-31 11:51:51 -0300

    fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "b78a47e5f35e78f535ca057e9026508c",
-"index.html": "004ee7ca0c1ba514d7cbf8113d02c8bb",
-"/": "004ee7ca0c1ba514d7cbf8113d02c8bb",
-"main.dart.js": "2f77f58b1ac3932a4bda28e8fc34335b",
+"index.html": "69f1a42d3fbf8c9d7140de69608425ec",
+"/": "69f1a42d3fbf8c9d7140de69608425ec",
+"main.dart.js": "4b0812615e1cde75521edb72f80d4eeb",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",

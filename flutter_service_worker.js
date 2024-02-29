ab2aafc27fd745dba7e6b3c56fb4ad6632563f46
Jose Moran <jose.moran@kenility.com>
2024-02-29 10:07:34 -0400

    adding new docs for camera

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "b78a47e5f35e78f535ca057e9026508c",
-"index.html": "8c01b59086216d4d8c615d17da693ebd",
-"/": "8c01b59086216d4d8c615d17da693ebd",
-"main.dart.js": "31817dd88b2a2fd82862f998738ace60",
+"index.html": "edca0b78799bc42d4c9680ecfe30ed9a",
+"/": "edca0b78799bc42d4c9680ecfe30ed9a",
+"main.dart.js": "c8cdecdd2353ec6c0280efd3ffd8d10e",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "6d247839db4baafd936776a388d35f17",
 "icons/Icon-192.png": "f616ad9051db4849263a010313f8dcf8",

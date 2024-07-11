d596f78e710dcede5d78de72d8ac732c83d82b95
federico-amura-kenility <74870582+federico-amura-kenility@users.noreply.github.com>
2024-07-11 16:55:53 -0300

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2611651676"
+    serviceWorkerVersion: "1737812350"
   }
 });
